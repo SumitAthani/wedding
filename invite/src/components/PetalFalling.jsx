9b86316f696f7e1dc0429513fd9055304d6afcb7
sphoortimpatil <sphoortimpatil@gmail.com>
2023-03-18 23:43:26 +0530

    added confetti

--- invite/src/components/PetalFalling.jsx
+++ invite/src/components/PetalFalling.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
+import {Snow} from "react-snow-particle"
+
 
 function PetalFalling() {
   const styles = {
@@ -10,7 +12,11 @@ function PetalFalling() {
     },
   };
 
-  return <div style={styles.box}>petals</div>;
+  return <>
+   <Snow color="#000000" particles = "200" />
+   {/* <Snow backgroundImage="https://firebasestorage.googleapis.com/v0/b/pinboard-25.appspot.com/o/ezgif.com-resize.gif?alt=media&token=12dae8c9-00d1-4fc8-9fff-5f1e8ba3e01d" color="#000000" particles = "200" /> */}
+  <div style={styles.box}>petals</div>;
+  </>
 }
 
 export default PetalFalling;

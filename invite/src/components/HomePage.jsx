259b722822ca6234c87e9d4aeb0fd75a51ac73be
sphoortimpatil <sphoortimpatil@gmail.com>
2023-03-19 01:07:21 +0530

    added opacity

--- invite/src/components/HomePage.jsx
+++ invite/src/components/HomePage.jsx
@@ -1,27 +1,47 @@
 import React from 'react'
 import backgroundImage from "../assets/background.JPG";
-
+import { Snow } from 'react-snow-particle/lib';
 
 export default function HomePage() {
 
     const styles = {
         backgroungImage: {
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundPosition: 'center',
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          height: "100vh",
-          width: "100vw",
-          backgroundSize: "cover",
-          postion: "relative",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundPosition: 'center',
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            height: "100vh",
+            width: "100vw",
+            backgroundSize: "cover",
+            postion: "relative",
+            opacity: 0.5,
+
+            //   alpha: 0.7,
+
         },
+        backgroundColor: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "#000000",
+        },
+
     }
-  return (
-    <div  style={styles.backgroungImage}>HomePage</div>
-  )
+    return (
+        <>
+
+            <div style={styles.backgroundColor}>
+
+                <div style={styles.backgroungImage}>
+                    <Snow color="#000000" particles="100" />
+                </div>
+            </div>
+        </>
+
+    )
 
 
 }

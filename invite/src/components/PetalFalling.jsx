f9c09cab5f0d07a7412f8b46ea2a9ec5bd6445c0
sphoortimpatil <sphoortimpatil@gmail.com>
2023-03-18 23:27:43 +0530

    changed particle count in confetti

--- invite/src/components/PetalFalling.jsx
+++ invite/src/components/PetalFalling.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Snow } from 'react-snow-particle'
 
 function PetalFalling() {
   const styles = {
@@ -10,7 +11,10 @@ function PetalFalling() {
     },
   };
 
-  return <div style={styles.box}>petals</div>;
+  return <>
+  <div style={styles.box}>petals</div>;
+  <Snow backgroundImage="https://firebasestorage.googleapis.com/v0/b/pinboard-25.appspot.com/o/ezgif.com-resize.gif?alt=media&token=12dae8c9-00d1-4fc8-9fff-5f1e8ba3e01d" color="#000000" particles = "200" />
+  </>
 }
 
 export default PetalFalling;

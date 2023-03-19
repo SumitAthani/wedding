11d62fa0431acbb1846d1968bda41f9fe9e20904
sumit-a-zluri <sumit.a@zluri.com>
2023-03-19 22:42:44 +0530

    im

--- invite/src/components/PhotoGallery.jsx
+++ invite/src/components/PhotoGallery.jsx
@@ -1,13 +1,43 @@
 import PhotoAlbum from "react-photo-album";
+import image1 from "../assets/IMG1.jpg";
+import image2 from "../assets/IMG2.jpg";
+import image3 from "../assets/IMG3.jpg";
+import image4 from "../assets/IMG4.jpg";
+import image5 from "../assets/IMG5.jpg";
+import image6 from "../assets/IMG6.jpg";
 
 const photos = [
-    { src: "../assets/background.jpg", width: 800, height: 600 },
-    // { src: "/images/image2.jpg", width: 1600, height: 900 },
+  {
+    src: image1,
+    width: 800,
+    height: 600,
+  },
+  {
+    src: image2,
+    width: 800,
+    height: 600,
+  },
+  {
+    src: image3,
+    width: 800,
+    height: 600,
+  },
+  {
+    src: image4,
+    width: 800,
+    height: 600,
+  },
+  {
+    src: image5,
+    width: 800,
+    height: 600,
+  },
+
+  // { src: "/images/image2.jpg", width: 1600, height: 900 },
 ];
 
 export default function PhotoGallery() {
-    return <PhotoAlbum layout="rows" photos={photos} />;
+  return <PhotoAlbum layout="columns" photos={photos} key="sumit" />;
 }
-
 
 // "../assets/background.jpg"

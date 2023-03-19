17fafcccd62d0f1de7a06971a6f3c721d9c1cf8d
sphoortimpatil <sphoortimpatil@gmail.com>
2023-03-19 20:58:27 +0530

    Added HomePageWrapper

--- invite/src/components/HomePage.jsx
+++ invite/src/components/HomePage.jsx
@@ -4,14 +4,16 @@ import "../css/HomePage.css";
 export default function HomePage() {
   return (
     <>
-      <div className="backgroundColor">
-        <div className="backgroungImage"></div>
-        <div className="homepageText">
-          <h1>This is us !</h1>
-          <h2>Little bit crazy , </h2>
-          <h2>Little bit loud</h2>
-          <h2>and a whole lot of</h2>
-          <h1>Love ❤️</h1>
+      <div className="homePageWrapper">
+        <div className="backgroundColor">
+          <div className="backgroungImage"></div>
+          <div className="homepageText">
+            <h1>This is us !</h1>
+            <h2>Little bit crazy , </h2>
+            <h2>Little bit loud</h2>
+            <h2>and a whole lot of</h2>
+            <h1>Love ❤️</h1>
+          </div>
         </div>
       </div>
     </>

ac3f5091ad593181723005fb3cdf23703e134506
sphoortimpatil <sphoortimpatil@gmail.com>
2023-03-20 00:32:02 +0530

    changed animation size

--- invite/src/components/Footer.jsx
+++ invite/src/components/Footer.jsx
@@ -3,22 +3,22 @@ import lottie from "lottie-web";
 import cat from "../assets/animation/catMeow.json";
 
 export default function Footer() {
-  useEffect(() => {
-    const instance = lottie.loadAnimation({
-      container: document.querySelector("#cat-animation"),
-      animationData: cat,
-      loop: true, // boolean
-      autoplay: true, // boolean
-    });
-    return () => instance.destroy();
-  }, []);
+    useEffect(() => {
+        const instance = lottie.loadAnimation({
+            container: document.querySelector("#cat-animation"),
+            animationData: cat,
+            loop: true, // boolean
+            autoplay: true, // boolean
+        });
+        return () => instance.destroy();
+    }, []);
 
-  return (
-    <>
-      <div className="footer">
-        {/* <p>This is some content in sticky footer</p> */}
-        <div id="cat-animation" style={{ width: "20%", height: "20%" }} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="footer">
+                {/* <p>This is some content in sticky footer</p> */}
+                <div id="cat-animation" style={{ width: 100, height: 75, marginTop: -10, marginLeft: -15 }} />
+            </div>
+        </>
+    );
 }
